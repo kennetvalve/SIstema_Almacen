4c1a4dde7ae8fe4fba746bfffa472ad9662f3870
Kennet Valverde <k90_rock@outlook.es>
2018-06-24 15:15:19 -0500

    v1.0

---
+++ SIstema_Almacen/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SIstema_Almacen
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
